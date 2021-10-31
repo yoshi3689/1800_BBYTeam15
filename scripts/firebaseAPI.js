e4296a45e2e2419a6f7916f83d1eaf7770e24019
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2021-10-31 15:30:30 -0700

    added a firebase auth to login.html and a separae js file

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCs-9tQ7DpU22iIAzwg9A5runtRj9yj8Nk",
+    authDomain: "comp1800-group.firebaseapp.com",
+    projectId: "comp1800-group",
+    storageBucket: "comp1800-group.appspot.com",
+    messagingSenderId: "993666205533",
+    appId: "1:993666205533:web:b731576d723bd78e22cab4"
+  };
+
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);

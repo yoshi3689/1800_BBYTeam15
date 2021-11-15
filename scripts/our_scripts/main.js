f3850d85d40167d23cef6c1983fef7498248a568
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2021-11-14 19:58:34 -0800

    solving merge conflicts

--- scripts/our_scripts/main.js
+++ scripts/our_scripts/main.js
@@ -1,3 +1,21 @@
-console.log('hello, it is main');
+console.log('connected')
+const tipList = document.getElementById('dailyTips');
+const storage = window.localStorage;
 
+console.log(document.referrer)
+
+const transferTip = e => {
+  if (e.target.innerHTML.includes('<')) {
+    const tipId = e.target.children.item(1).id;
+    console.log(tipId);
+    // if the tip is not stored in the local storage, it creates a new tip key in there
+    if (storage.getItem(tipId) == null) {
+      storage.setItem(tipId, tipId);
+      console.log(tipId + " stored successfully");
+    }
+  }
+  
+}
+
+tipList.addEventListener('click', transferTip);
 // const userName_ava = document.getElementById('user-name_avatar');
